91d09babd8280b411fe6d5b971c70d95bb6e4cb1
Mehdi Khalfallah <contact.mehdi.khalfallah@gmail.com>
2022-06-02 14:14:57 +0100

    if statements

--- app/src/main/java/com/example/kotlinbasics/basics.kt
+++ app/src/main/java/com/example/kotlinbasics/basics.kt
@@ -2,8 +2,27 @@ package com.example.kotlinbasics
 
 fun main () {
     //TODO : add new functionnality
- var name = "Denis"
-     name = "mehdi"
-print("hello " + name )
-}
+
+    var result = 5+3
+     result /= 2
+println("hello " + result )
+    var isequal = 5 ==5
+
+    println("equality ${5==5}")
+    var person1 = 182
+    var person2 =181
+
+    if(person1 > person2) {
+        println("person1 wins") }
+        else if (person1 == person2 ) {
+
+            println("both are equal")
+        }
+        else {
+            println("easy")
+        }
+
+
+    }
+
 
